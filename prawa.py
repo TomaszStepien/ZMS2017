5478661117c81c089d11ef97256d641899dd6a7e
TomaszStepien <tomaszstepien03@gmail.com>
2017-06-03 13:22:47 +0200

    biedna wersja swiatel z usunietymi swiatlami

---
+++ prawa.py
@@ -0,0 +1,50 @@
+import random
+import simpy
+# import numpy
+
+# source generates new cars randomly
+def source(env, number, interval, intersection):
+    for i in range(number):  # liczba samochodow w symulacji
+        j = random.choice([0,1,2,3]) # wybor trasy z ktorej samochod przyjezdza
+        c = car(env, 'Car%02d%02d' % (i,j), intersection)
+        env.process(c)
+        # t = random.expovariate(1.0 / interval) # czestotliwosc pojawiania sie aut
+        t = 5
+        yield env.timeout(t)
+
+
+def car(env, name, intersection):
+    """car arrives, is served and leaves"""
+    arrive = env.now
+
+    print('%7.4f %s: Here I am' % (arrive, name))
+
+    with intersection.request() as req:
+        yield req
+        wait = env.now - arrive
+
+        # We got to the rondo
+        print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
+        tir = 5.00 + wait  # random.expovariate(1.0 / time_in_bank)
+        yield env.timeout(tir)
+        print('%7.4f %s: Finished' % (env.now, name))
+
+
+# Setup and start the simulation
+
+random_seed = 42
+new_cars = 5  # Total number of cars
+interval_cars = 10.0  # Generate new cars roughly every x seconds
+
+print('RONDO')
+random.seed(random_seed)
+env = simpy.Environment()
+
+# Start processes and run
+intersection = simpy.Resource(env, capacity=1) # ile samochodow moze naraz znajdowac sie na rondzie
+env.process(source(env,
+                   number=new_cars,
+                   interval=interval_cars,
+                   intersection=intersection))
+env.run()
+
