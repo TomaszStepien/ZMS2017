fd78b2f5f10648ad85ed24792d51c3d80033d917
TomaszStepien <tomaszstepien03@gmail.com>
2017-06-07 20:57:08 +0200

    super upgrade

--- swiatla.py
+++ swiatla.py
@@ -7,41 +7,14 @@ import pandas as pd
 
 # source generates new cars randomly
 # and we need 4 of them because 4 directions of arrival are possible
-def source_east(env, number, interval, intersection, lights, times):
+def source(env, number, interval, intersection, lights, times, origin):
     for i in range(number):  # number of cars coming from this direction
-        c = car(env, 'Car%02dEast' % i, intersection, lights, times)
+        c = car(env, 'Car%02d%s' % (i, origin), intersection, lights, times)
         env.process(c)
         # t = random.expovariate(1.0 / interval) # czestotliwosc pojawiania sie aut
         t = 5
         yield env.timeout(t)
-
-
-def source_west(env, number, interval, intersection, lights, times):
-    for i in range(number):
-        c = car(env, 'Car%02dWest' % i, intersection, lights, times)
-        env.process(c)
-        # t = random.expovariate(1.0 / interval) # czestotliwosc pojawiania sie aut
-        t = 5
-        yield env.timeout(t)
-
-
-def source_north(env, number, interval, intersection, lights, times):
-    for i in range(number):
-        c = car(env, 'Car%02dNorth' % i, intersection, lights, times)
-        env.process(c)
-        # t = random.expovariate(1.0 / interval) # czestotliwosc pojawiania sie aut
-        t = 5
-        yield env.timeout(t)
-
-
-def source_south(env, number, interval, intersection, lights, times):
-    for i in range(number):
-        c = car(env, 'Car%02dSouth' % i, intersection, lights, times)
-        env.process(c)
-        # t = random.expovariate(1.0 / interval)  # czestotliwosc pojawiania sie aut
-        t = 5
-        yield env.timeout(t)
-
+        times['origin'].append(origin)
 
 def car(env, name, intersection, lights, times):
     """car arrives, is served and leaves"""
@@ -49,7 +22,6 @@ def car(env, name, intersection, lights, times):
     times['arrival_time'].append(arrive)
     cykl = sum(lights)
     red_time = max(lights[0] - arrive % cykl, 0)  # zakladamy ze zaczyna sie od czerwonego
-
     print('%7.4f %s: Here I am' % (arrive, name))
 
     with intersection.request() as req:
@@ -69,20 +41,16 @@ def car(env, name, intersection, lights, times):
 # set parameters
 random_seed = 2137
 
-cars_east = 5  # how many cars coming from each direction
-cars_west = 5
-cars_north = 5
-cars_south = 5
-
-interval_east = 10.0  # Generate new cars roughly every x seconds
-interval_west = 10.0
-interval_north = 10.0
-interval_south = 10.0
+# for each origin there are: name, number of cars and interval
+origins = (('east', 5, 10),
+           ('west', 5, 10),
+           ('north', 5, 10),
+           ('south', 5, 10))
 
 capacity = 5  # How many cars can enter the intersection simultaneously
 
-lights1 = [20, 40]  # red, green
-lights2 = [40, 20]  # red, green
+lights1 = (20, 40)  # red, green
+lights2 = (20, 40)  # green, red
 
 # set up the environment
 print('Intersection with lights')
@@ -90,45 +58,22 @@ random.seed(random_seed)
 env = simpy.Environment()
 times = {'waiting_time': [],
          'total_time': [],
-         'arrival_time': []}
+         'arrival_time': [],
+         'origin': []}
 
 # Start processes and run
 intersection = simpy.Resource(env, capacity=capacity)
-env.process(source_east(env,
-                        number=cars_east,
-                        interval=interval_east,
-                        intersection=intersection,
-                        lights=lights1,
-                        times=times))
+for e in origins:
+    env.process(source(env,
+                       number=e[1],
+                       interval=e[2],
+                       intersection=intersection,
+                       lights=lights1,
+                       times=times,
+                       origin=e[0]))
 
-env.process(source_west(env,
-                        number=cars_west,
-                        interval=interval_west,
-                        intersection=intersection,
-                        lights=lights1,
-                        times=times))
-
-env.process(source_north(env,
-                         number=cars_north,
-                         interval=interval_north,
-                         intersection=intersection,
-                         lights=lights2,
-                         times=times))
-
-env.process(source_south(env,
-                         number=cars_south,
-                         interval=interval_south,
-                         intersection=intersection,
-                         lights=lights2,
-                         times=times))
 env.run()
 
 times = pd.DataFrame(times)
 
 print(times)
-
-
-
-
-
-
