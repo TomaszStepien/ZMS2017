7d98400553b224600383d55d8904e87a48c2000e
TomaszStepien <tomaszstepien03@gmail.com>
2017-06-07 23:20:33 +0200

    losowość

--- rondo.py
+++ rondo.py
@@ -3,22 +3,19 @@ import simpy
 import pandas as pd
 
 
-# import numpy
-
 # source generates new cars randomly
 def source(env, number, interval, rondo, times, origin, rondo_quarter=5.00):
     for i in range(number):  # number of cars coming from this direction
         c = car(env, 'Car%02d%s' % (i, origin), rondo, times, rondo_quarter, origin)
         env.process(c)
-        # t = random.expovariate(1.0 / interval) # czestotliwosc pojawiania sie aut
-        t = 5
+        t = random.expovariate(1.0 / interval) # frequency of car creation
         yield env.timeout(t)
 
 
 def car(env, name, rondo, times, rondo_quarter, origin):
-    """car arrives, is served and leaves"""
+    # car arrives, is served and leaves
     arrive = env.now
-    print('%7.4f %s: Here I am' % (arrive, name))
+    # print('%7.4f %s: Here I am' % (arrive, name))
 
     with rondo.request() as req:
         yield req
@@ -26,12 +23,13 @@ def car(env, name, rondo, times, rondo_quarter, origin):
 
         # We got to the rondo
         yield env.timeout(wait)
-        print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
+        # print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
         i = random.choice([1, 2, 3, 4])  # for each car a random exit from the roundabout is chosen
-        tir = i * rondo_quarter  # random.expovariate(1.0 / time_in_bank)
+        tir = i * rondo_quarter
         yield env.timeout(tir)
-        print('%7.4f %s: Finished' % (env.now, name))
+        # print('%7.4f %s: Finished' % (env.now, name))
 
+    # dictionary with results is updated
     times['total_time'].append(wait + tir)
     times['waiting_time'].append(wait)
     times['arrival_time'].append(arrive)
@@ -39,13 +37,16 @@ def car(env, name, rondo, times, rondo_quarter, origin):
 
 
 # set parameters
-random_seed = 2137
-
 # for each origin there are: name, number of cars and interval
-origins = [['east', 5, 10],
-           ['west', 5, 10],
-           ['north', 5, 10],
-           ['south', 5, 10]]
+origins = (('east', 100, 5),
+           ('west', 100, 5),
+           ('north', 100, 5),
+           ('south', 100, 5))
+
+times = {'waiting_time': [],
+         'total_time': [],
+         'arrival_time': [],
+         'origin': []}
 
 size = 1000  # the circumference of the roundabout - the bigger it is the more cars can enter
 #  but also the longer it takes to travel through it
@@ -54,12 +55,7 @@ rondo_quarter = size // 4  # how many seconds does it take to drive through 1/4 
 
 # set up the environment
 print('RONDO')
-random.seed(random_seed)
 env = simpy.Environment()
-times = {'waiting_time': [],
-         'total_time': [],
-         'arrival_time': [],
-         'origin': []}
 
 # Start processes and run
 rondo = simpy.Resource(env, capacity=capacity)
