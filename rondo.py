eaa111c3d3ecf98d0d1ddf7cd3415cc210e5b843
TomaszStepien <tomaszstepien03@gmail.com>
2017-06-07 22:37:44 +0200

    drobne zmiany

--- rondo.py
+++ rondo.py
@@ -6,20 +6,18 @@ import pandas as pd
 # import numpy
 
 # source generates new cars randomly
-# and we need 4 of them because 4 directions of arrival are possible
 def source(env, number, interval, rondo, times, origin, rondo_quarter=5.00):
     for i in range(number):  # number of cars coming from this direction
-        c = car(env, 'Car%02d%s' % (i, origin), rondo, times, rondo_quarter)
+        c = car(env, 'Car%02d%s' % (i, origin), rondo, times, rondo_quarter, origin)
         env.process(c)
         # t = random.expovariate(1.0 / interval) # czestotliwosc pojawiania sie aut
         t = 5
         yield env.timeout(t)
-        times['origin'].append(origin)
 
-def car(env, name, rondo, times, rondo_quarter):
+
+def car(env, name, rondo, times, rondo_quarter, origin):
     """car arrives, is served and leaves"""
     arrive = env.now
-    times['arrival_time'].append(arrive)
     print('%7.4f %s: Here I am' % (arrive, name))
 
     with rondo.request() as req:
@@ -28,13 +26,16 @@ def car(env, name, rondo, times, rondo_quarter):
 
         # We got to the rondo
         yield env.timeout(wait)
-        times['waiting_time'].append(wait)
         print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
         i = random.choice([1, 2, 3, 4])  # for each car a random exit from the roundabout is chosen
         tir = i * rondo_quarter  # random.expovariate(1.0 / time_in_bank)
         yield env.timeout(tir)
-        times['total_time'].append(wait + tir)
         print('%7.4f %s: Finished' % (env.now, name))
+
+    times['total_time'].append(wait + tir)
+    times['waiting_time'].append(wait)
+    times['arrival_time'].append(arrive)
+    times['origin'].append(origin)
 
 
 # set parameters
@@ -46,7 +47,7 @@ origins = [['east', 5, 10],
            ['north', 5, 10],
            ['south', 5, 10]]
 
-size = 13  # the circumference of the roundabout - the bigger it is the more cars it can fit
+size = 1000  # the circumference of the roundabout - the bigger it is the more cars can enter
 #  but also the longer it takes to travel through it
 capacity = size // 3  # How many cars can enter the roundabout simultaneously
 rondo_quarter = size // 4  # how many seconds does it take to drive through 1/4 of roundabout
@@ -77,4 +78,3 @@ env.run()
 times = pd.DataFrame(times)
 
 print(times)
-
