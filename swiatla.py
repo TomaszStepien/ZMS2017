9d9b2bd320e8a5e8adcffafe0d18357ca470dc9b
TomaszStepien <tomaszstepien03@gmail.com>
2017-06-07 22:38:49 +0200

    dodanie 2 cyklów

--- swiatla.py
+++ swiatla.py
@@ -6,22 +6,23 @@ import pandas as pd
 # import numpy
 
 # source generates new cars randomly
-# and we need 4 of them because 4 directions of arrival are possible
 def source(env, number, interval, intersection, lights, times, origin):
     for i in range(number):  # number of cars coming from this direction
-        c = car(env, 'Car%02d%s' % (i, origin), intersection, lights, times)
+        c = car(env, 'Car%02d%s' % (i, origin), intersection, lights, times, origin)
         env.process(c)
         # t = random.expovariate(1.0 / interval) # czestotliwosc pojawiania sie aut
         t = 5
         yield env.timeout(t)
-        times['origin'].append(origin)
 
-def car(env, name, intersection, lights, times):
+
+def car(env, name, intersection, lights, times, origin):
     """car arrives, is served and leaves"""
     arrive = env.now
-    times['arrival_time'].append(arrive)
     cykl = sum(lights)
-    red_time = max(lights[0] - arrive % cykl, 0)  # zakladamy ze zaczyna sie od czerwonego
+    if origin in ('north', 'south'):
+        red_time = max(lights[0] - (arrive % cykl), 0)  # cycle starts with red
+    else:
+        red_time = max(-(lights[0] - (arrive % cykl)), 0)  # cycle starts with green
     print('%7.4f %s: Here I am' % (arrive, name))
 
     with intersection.request() as req:
@@ -30,12 +31,15 @@ def car(env, name, intersection, lights, times):
 
         # We got to the intersection
         yield env.timeout(wait)
-        times['waiting_time'].append(wait)
         print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
         tir = 5.00  # random.expovariate(1.0 / time_in_bank)
         yield env.timeout(tir)
-        times['total_time'].append(wait + tir)
         print('%7.4f %s: Finished' % (env.now, name))
+
+    times['total_time'].append(wait + tir)
+    times['waiting_time'].append(wait)
+    times['arrival_time'].append(arrive)
+    times['origin'].append(origin)
 
 
 # set parameters
@@ -49,8 +53,7 @@ origins = (('east', 5, 10),
 
 capacity = 5  # How many cars can enter the intersection simultaneously
 
-lights1 = (20, 40)  # red, green
-lights2 = (20, 40)  # green, red
+lights = (20, 40)  # red, green
 
 # set up the environment
 print('Intersection with lights')
@@ -68,7 +71,7 @@ for e in origins:
                        number=e[1],
                        interval=e[2],
                        intersection=intersection,
-                       lights=lights1,
+                       lights=lights,
                        times=times,
                        origin=e[0]))
 
